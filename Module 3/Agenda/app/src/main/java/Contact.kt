ec3d5b22663c95c9f89197bf2b900ab163b812d6
Laripiauilino <larissaspiauilino@gmail.com>
2021-06-20 15:34:40 -0300

    Reorganizing files

--- Module 3/Agenda/app/src/main/java/Contact.kt
+++
@@ -1,6 +0,0 @@
-package com.larissa.agenda
-
-open class Contact(open val name: String,
-                   open val phone: String,
-                   open var dataType: DataType? = null){
-}
--- Module 3/Agenda/app/src/main/java/DataType.kt
+++
@@ -1,6 +0,0 @@
-package com.larissa.agenda
-
-enum class DataType(val id: Int, val description: String) {
-    PERSONAL(0, "pessoal"),
-    PROFESSIONAL(1, "profissional"),
-}
